626c1104db2cd9b40c16b15c2ae0f6ada6cfdf2d
Rafael Rocha <rafael07fla@gmail.com>
2024-11-14 16:10:31 -0300

    Exercicio Resolvidos

---
+++ Exercicios/java script/lib/exercicio9.js
@@ -0,0 +1,13 @@
+function  potencia(x,y)  { 
+
+    var n1 = Number(x)
+    var n2 = Number(y)
+    var resultado = n1 % n2
+    
+    return resultado }
+    
+    console.log(potencia(20,3))
+    console.log(potencia(25,5))
+    console.log(potencia(100,10))
+    console.log(potencia(100,30))
+    console.log(potencia(100,33))
