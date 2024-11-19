6c06d3eb4cb36f92e9b4c382f5b77f5975f6e015
Rafael Rocha <rafael07fla@gmail.com>
2024-11-19 16:50:56 -0300

    Lista 3 - exercicio2

---
+++ Exercicios/java script/lib/Lista_3/exercicio2.js
@@ -0,0 +1,10 @@
+function numero(par) {
+    if (par % 2 === 0) {
+        return "Par"
+    } else
+    return "impar"
+}
+console.log(numero(4))
+console.log(numero(7))
+console.log(numero(0))
+console.log(numero(-5))
