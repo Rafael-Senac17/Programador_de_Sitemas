59ea045ac5e91f18ec52294637e06c65c92c62a5
Rafael Rocha <rafael07fla@gmail.com>
2024-11-21 17:33:26 -0300

    Lista 3 - exercicio10

---
+++ Exercicios/java script/lib/Lista_3/exercicio10.js
@@ -0,0 +1,16 @@
+function soma(n) {
+    var x = 0
+    var y2 = 0
+    while (x < n ) {
+ 
+    var x = x + 1
+    
+ var y2 = y2 + x
+ console.log(y2)
+
+ return y2
+ }}
+soma(5)
+soma(10)
+soma(1)
+soma(64)
