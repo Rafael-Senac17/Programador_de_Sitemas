08a8c5632ca3bc0a76e9a55d64c5541721be87a9
Rafael Rocha <rafael07fla@gmail.com>
2024-11-25 14:56:18 -0300

    Lista3 - exercicio7

--- Exercicios/java script/lib/Lista_3/exercicio7.js
+++ Exercicios/java script/lib/Lista_3/exercicio7.js
@@ -1,10 +1,14 @@
 function bissexto(ano) {
-    if (ano % 4 === 0 && ano % 100 != 0) {
-        return "True"
-    } else {
-        return "False"
-    }
-}
+     if(ano % 400 === 0) return true
+
+     if (ano % 100 === 0) return false
+
+     if (ano % 4 === 0) return true
+
+       return false;  
+
+ }
+ 
 console.log(bissexto(2020)) 
 console.log(bissexto(2024)) 
 console.log(bissexto(1999)) 
