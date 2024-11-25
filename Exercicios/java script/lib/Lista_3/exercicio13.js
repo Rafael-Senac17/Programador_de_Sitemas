f812423483dff96804775d256a7e1c5335d7280b
Rafael Rocha <rafael07fla@gmail.com>
2024-11-25 13:41:59 -0300

    Lista 3 - exercicio13

--- Exercicios/java script/lib/Lista_3/exercicio13.js
+++ Exercicios/java script/lib/Lista_3/exercicio13.js
@@ -1,20 +1,18 @@
 
 for (var x = 1; x <= 10; x ++ ) {
     
-    var n = 1
- while (n <= 10) {
+   for (var n = 1; n <= 10; n++) {
         
-var n = n++
  var n2 = n * x 
+ console.log(n2)
 }
 
 }    
 
 
-console.log(n2)
        
  //console.log(n)
- //console.log(n)
+ //console.log(x)
 
  
 
