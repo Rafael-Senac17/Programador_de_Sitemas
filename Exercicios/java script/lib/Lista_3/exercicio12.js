47405caf9fd65109af2fa22551a5d0af2fb6872d
Rafael Rocha <rafael07fla@gmail.com>
2024-11-25 15:16:54 -0300

    Lista 3 - exercicio12

--- Exercicios/java script/lib/Lista_3/exercicio12.js
+++ Exercicios/java script/lib/Lista_3/exercicio12.js
@@ -2,8 +2,8 @@ function tabuada(n) {
     for (var x = 0; x <= 10; x ++ ) {
     
    var n2 = n * x
-  let linha = `${n} x ${x}`
-  
+  let linha = `${n} x ${x} = ${n2}`
+
   console.log(linha)
 
 
