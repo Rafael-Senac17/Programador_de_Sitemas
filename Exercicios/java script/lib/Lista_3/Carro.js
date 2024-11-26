136723f29427cb7db19aad75b7152c3c7493b302
Rafael Rocha <rafael07fla@gmail.com>
2024-11-26 17:25:11 -0300

    Carro

---
+++ Exercicios/java script/lib/Lista_3/Carro.js
@@ -0,0 +1,23 @@
+ var carro2 = {
+
+marca: "Ford"
+,modelo: "Maverick v8"
+,ano: 1973
+,fabricacao: "Taubaté"
+,classe: "Compacto"
+,cor: "Preto"
+,Motor: "6 cilindros"
+,potencia: "199 cv"
+,combustao:"Flex"
+,direcao: "Hidráulica"
+,portas:4
+,carroceria: "Sedã"
+,calimbragem: {dianteiro:30, traseiro: 30, step: 32}
+,cambio: "Manual, 4 marchas"
+,chassi:"LB 5DNE 20758"
+,proprietario:"Rafael"
+,tracao: "AWD"
+,aro: 14
+ }
+
+ console.log(carro2)
