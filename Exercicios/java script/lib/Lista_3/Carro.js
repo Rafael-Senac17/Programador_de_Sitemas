6e1232bb40e99626742759f876f7afb50cca1f52
Rafael Rocha <rafael07fla@gmail.com>
2024-11-28 16:05:16 -0300

    Carro

--- Exercicios/java script/lib/Lista_3/Carro.js
+++ Exercicios/java script/lib/Lista_3/Carro.js
@@ -3,6 +3,20 @@ var carro1 = {
     modelo: "Maverick v8"
     , cor: "Preto"
     , velocidade: 0
+    , acelerar: function () {
+        this.velocidade += 5
+
+        console.log("Acelerou para " + this.velocidade)
+    }
+    , frear: function () {
+        this.velocidade -= 13
+        if (this.velocidade < 0) {
+            this.velocidade = 0
+        }
+    }
+    , imprimir: function () {
+        console.table(this);
+    }
 
 
 }
@@ -10,30 +24,32 @@ var carro2 = {
     modelo: "Ferrari"
     , cor: "Preto"
     , velocidade: 0
-}
+    , acelerar: function () {
+        this.velocidade += 5
 
-
-function acelerar(carro) {
-
-    carro.velocidade += 5
-}
-
-while (carro1.velocidade < 80) acelerar(carro1)
-
-
-
-function desacelerar(carro) {
-    carro.velocidade -= 13
-    if (carro.velocidade < 0) {
-        carro.velocidade = 0
+        console.log("Acelerou para " + this.velocidade)
+    }
+    , frear: function () {
+        this.velocidade -= 13
+        if (this.velocidade < 0) {
+            this.velocidade = 0
+        }
+    }
+    , imprimir: function () {
+        console.table(this);
     }
 }
-while (carro1.velocidade > 0) desacelerar(carro1)
 
-    while (carro2.velocidade > 0) desacelerar(carro2)
-        
-        
-        console.log(carro1.velocidade)
+while (carro1.velocidade < 80) carro1.acelerar()
+while (carro2.velocidade < 110) carro2.acelerar()
 
 
-console.log(carro2.velocidade)
+while (carro1.velocidade > 0) carro1.frear()
+while (carro2.velocidade > 0) carro2.frear()
+
+carro1.imprimir()
+carro2.imprimir()
+// console.log(carro1.velocidade)
+
+
+// console.log(carro2.velocidade)
