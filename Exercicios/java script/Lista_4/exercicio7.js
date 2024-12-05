e55a5435add90978de0597b41ded7d446fea1963
Rafael Rocha <rafael07fla@gmail.com>
2024-12-05 17:13:36 -0300

    Lista 4 - Primeira Parte

---
+++ Exercicios/java script/Lista_4/exercicio7.js
@@ -0,0 +1,14 @@
+var lista1 = [1, 2, 3, 4, 5]
+var lista2 =  ["a", "b", "c"]
+var lista3 = [10]
+
+
+
+lista1.push(6)
+lista2.push("d")
+lista3.push(20)
+
+
+console.log(lista1)
+console.log(lista2)
+console.log(lista3)
