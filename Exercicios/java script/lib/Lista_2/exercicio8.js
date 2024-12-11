c23e2e0095c859f6ee1eb7b096cae12a526a781e
Rafael Rocha <rafael07fla@gmail.com>
2024-12-11 14:53:08 -0300

    Lista 2

---
+++ Exercicios/java script/lib/Lista_2/exercicio8.js
@@ -0,0 +1,13 @@
+function  potencia(base)  { 
+
+    var n1 = Number(base)
+    var resultado = n1 ** 2
+    
+    return resultado }
+    
+    console.log(potencia(2))
+    console.log(potencia(5))
+    console.log(potencia(10))
+    console.log(potencia(0.5))
+    console.log(potencia(0.25))
+    
