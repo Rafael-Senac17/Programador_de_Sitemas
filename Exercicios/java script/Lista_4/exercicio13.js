93da0fdd326a172ce8020bc9f0ad26e02faeadc2
Rafael Rocha <rafael07fla@gmail.com>
2024-12-11 14:49:03 -0300

    Lista 4

--- Exercicios/java script/Lista_4/exercicio13.js
+++ Exercicios/java script/Lista_4/exercicio13.js
@@ -1,9 +1,9 @@
-function maior(lista){
-for(var x = 0; x < lista.length;x++){
+var lista = [1, 2, 3, 4, 5]
+var lista2 = [-1, -2, -3, -4, -5]
+var lista3 = [10, 20, 30, 40, -50]
+var lista4 = []
 
-lista[x] >= lista[x]
-}}
-
-
-
-maior([5,2,3,4])
+console.log(Math.max.apply(null, lista))
+console.log(Math.max.apply(null, lista2))
+console.log(Math.max.apply(null, lista3))
+console.log(Math.max.apply(null, lista4))
---
+++ Exercicios/java script/Lista_4/exercicio15.js
@@ -0,0 +1,3 @@
+console.log("hello".split('').reverse().join(''))
+console.log("world".split('').reverse().join(''))
+console.log("abc".split('').reverse().join(''))
