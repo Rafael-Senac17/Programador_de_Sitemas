6d7948c267bdce54d48f656a35665a767f257e13
Rafael Rocha <rafael07fla@gmail.com>
2024-12-12 16:26:50 -0300

    Lista

--- Exercicios/java script/lib/Lista_4/exercicio11.js
+++ Exercicios/java script/lib/Lista_4/exercicio11.js
@@ -1,16 +1,12 @@
-function media(lista) {
-    var soma = 0;
-    for (var x = 0; x < lista.length; x++) {
-        soma += lista[x];
-    } if (soma != 0) {
-        var mediana = soma / x
-        console.log(mediana)
-    } else {
-        console.log(soma)
-    }
+function soma(a,b) {
+return a + b
 }
 
-media([1, 2, 4, 5, 7, 8, 10, 11])
-media([10, 70, 30, 50])
-media([29, 567, 38, 53, 503, 92, 67, 909, 203, 382, 28])
-media([])
+var lis = "3 7 9 10 13"
+console.log(lis.split(' ').map(Number).reduce((a ,b ,Index ,lis) => a + b / lis.length, 0))
+var lis2 = "10 70 30 50"
+console.log(lis2.split(' ').map(Number).reduce((a ,b ,Index ,lis) => a + b / lis.length, 0))
+var lis3 = "29 567 38 53 503 92 67 909 203 382 28"
+console.log(lis3.split(' ').map(Number).reduce((a ,b ,Index ,lis) => a + b / lis.length, 0))
+var lis4 = "1 2 4 5 7 8 10 11"
+console.log(lis4.split(' ').map(Number).reduce((a ,b ,Index ,lis) => a + b / lis.length, 0))
