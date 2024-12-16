5ad759b9be75220b3bca03c407f444d1d691a493
Rafael Rocha <rafael07fla@gmail.com>
2024-12-16 15:20:14 -0300

    Lista 4

--- Exercicios/java script/lib/Lista_4/exercicio17.js
+++ Exercicios/java script/lib/Lista_4/exercicio17.js
@@ -1,10 +1,5 @@
-function verificacao(x) {
-    if (isNaN(x)) {
-      return "False";
-    }
-    return "True";
-  }
+import { verificacao } from "../Biblioteca.js"
 
-  console.log(verificacao("12345"))
-  console.log(verificacao("abc123"))
-  console.log(verificacao("67890"))
+console.log(verificacao("12345"))
+console.log(verificacao("abc123"))
+console.log(verificacao("67890"))
--- Exercicios/java script/lib/Lista_4/exercicio19.js
+++ Exercicios/java script/lib/Lista_4/exercicio19.js
@@ -1,9 +1,4 @@
-function Anagrama(a, b) {
-    var x = a.split('').sort().join('')
-    var y = b.split('').sort().join('')
-
-    return x === y
-}
+import { Anagrama } from "../Biblioteca.js"
 
 console.log(Anagrama("listen","silent"))
 console.log(Anagrama("hello","world"))
