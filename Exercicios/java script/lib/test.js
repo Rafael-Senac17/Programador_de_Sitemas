a93520ea2cf03ce7a2da8f65f9873b735c70e237
Rafael Rocha <rafael07fla@gmail.com>
2024-12-16 13:56:14 -0300

    teste

---
+++ Exercicios/java script/lib/test.js
@@ -0,0 +1,27 @@
+var a;
+var a = 10;
+    a = 20;
+var a = 30;
+console.log(a)
+
+let b;
+//let b = 40;
+    b = 50;
+//let b = 60;
+console.log(b)
+
+//const c;
+const c = 70
+     // c = 80;
+//const c = 90;
+console.log(c)
+
+
+// const a = 120
+// let a = 100
+// var b = 100
+// var c = 100
+// let c = 101
+// const b = 101
+
+console.log(a)
