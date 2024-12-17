5693e9f20f722f3bb3823f7ec15c6c837a920d91
Rafael Rocha <rafael07fla@gmail.com>
2024-12-17 16:56:54 -0300

    Lista 2

--- Exercicios/java script/lib/Lista_2/Biblioteca.js
+++ Exercicios/java script/lib/Lista_2/Biblioteca.js
@@ -1,76 +1,77 @@
 "Biblioteca - Lista 2"
 
+//Lista 2 - Exercicio 2 e 7
 export function  nome(x,y)  { 
 return `${x} ${y}`}
-
+//Lista 2 - Exercicio 3
 export function  soma(x,y)  { 
 var n1 = Number(x)
 var n2 = Number(y)
 var resultado = n1 + n2
 return resultado }
-
+//Lista 2 - Exercicio 4
 export function  multiplicacao(x,y)  { 
     var n1 = Number(x)
     var n2 = Number(y)
     var resultado = n1 * n2
 return resultado }
-
+//Lista 2 - Exercicio 5
 export function  media(x,y,z)  { 
     var n1 = Number(x)
     var n2 = Number(y)
     var n3 = Number(z)
     var resultado = (n1 + n2 + n3) / 3
 return resultado }
-
+//Lista 2 - Exercicio 6
 export function  divisao(x,y)  { 
     var n1 = Number(x)
     var n2 = Number(y)
     var resultado = n1 / n2
 return resultado }
-
+//Lista 2 - Exercicio 8
 export function  potencia(base)  { 
     var n1 = Number(base)
     var resultado = n1 ** 2
 return resultado }
-
+//Lista 2 - Exercicio 9
 export function  resto(x,y)  { 
     var n1 = Number(x)
     var n2 = Number(y)
     var resultado = n1 % n2
 return resultado }
-
+//Lista 2 - Exercicio 10
 export function  calcularArea(largura,altura)  { 
     var n1 = Number(largura)
     var n2 = Number(altura)
     var resultado = n1 * n2
 return resultado }
-
+//Lista 2 - Exercicio 11
 export function  potencia2(base,expoente)  { 
     var n1 = Number(base)
     var n2 = Number(expoente)
     var resultado = n1 ** n2
 return resultado }
-
+//Lista 2 - Exercicio 12
 export function  dobro(numero)  { 
     var n1 = Number(numero)
     var resultado = n1 * 2
 return resultado }
-
+//Lista 2 - Exercicio 13
 export function  raizQuadrada(numero)  { 
     var n1 = Number(numero)
     var resultado = Math.sqrt(n1)
 return resultado }
-
+//Lista 2 - Exercicio 14
 export function  perimetro(numero)  { 
     var n1 = Number(numero)
     var resultado = n1 * 2
 return resultado }
-
+//Lista 2 - Exercicio 15
 export function  fahrenheit(numero)  { 
     var n1 = Number(numero)
     var resultado = n1 * 1.8 + 32
 return resultado }
-
+//Lista 2 - Exercicio 16
 export function  volumeEsfera(numero)  { 
     var n1 = Number(numero)
     var resultado =(n1 ** 3) * 4 / 3
